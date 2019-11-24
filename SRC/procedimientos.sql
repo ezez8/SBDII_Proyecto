b6b11ef1e0d1a51c094fa63340d21d123b1a50b9
SatoshiKiddo <elesfuerzodeminami21@hotmail.com>
2019-11-24 07:24:44 -0400

    Se agregaron todos los procedimientos fijos y triggers de validacion, falta agregar el status para cada unidad de uso dentro de la base de datos

---
+++ SRC/procedimientos.sql
@@ -0,0 +1,116 @@
+
+
+CREATE OR REPLACE TYPE usuarios AS VARRAY(10) OF NUMBER;
+/
+CREATE OR REPLACE PROCEDURE generar_plan_viaje (fechaIn DATE, id_usuario_comprador number, acompanantes SYSTEM.usuarios, length_acomp number)
+IS
+    id_flag number;
+BEGIN
+    COMMIT;
+    INSERT INTO PLAN_VIAJE (pv_fecha, pv_precio_total) VALUES (SYSDATE, 0);
+    id_flag := seq_pv_id.CURRVAL;
+    INSERT INTO PLAN_USUARIO (pu_comprador, pu_pv_id, pu_u_id) VALUES (1, id_flag, id_usuario_comprador);
+    FOR i IN 1..length_acomp LOOP
+        INSERT INTO PLAN_USUARIO (pu_comprador, pu_pv_id, pu_u_id) VALUES (0, id_flag, acompanantes(i));    
+    END LOOP;
+END;
+/
+CREATE OR REPLACE PROCEDURE generar_vuelo (id_aeropuerto_origen number, id_aeropuerto_destino number,
+fecha_in DATE,
+fecha_out DATE,
+duracion number,
+vu_stat varchar,
+precio_ej number,
+precio_ee number,
+precio_cp number
+)
+IS
+BEGIN
+    INSERT INTO vuelo (vu_fecha, vu_duracion, vu_status, vu_precio_ej, vu_precio_ee, vu_precio_cp) VALUES (reg_ope(fecha_in, fecha_out), duracion, reg_sta(vu_stat), precio_ej, precio_ee, precio_cp);
+    INSERT INTO nodo (no_modo, no_status, no_ap_id, no_vu_id) VALUES ('ORI', reg_sta('ACT'), id_aeropuerto_origen, seq_vu_id.CURRVAL);
+    INSERT INTO nodo (no_modo, no_status, no_ap_id, no_vu_id) VALUES ('DES', reg_sta('ACT'), id_aeropuerto_origen, seq_vu_id.CURRVAL);
+END;
+/
+CREATE OR REPLACE PROCEDURE generar_reserva_vuelo 
+(id_vuelo number,
+clase varchar,
+id_unidad_avion number,
+tipo varchar, 
+modo varchar, 
+status varchar,
+id_plan_viaje number)
+IS
+CURSOR asiento_disp IS SELECT * FROM asiento WHERE asi_ua_id = id_unidad_avion AND asi_clase = clase;
+asiento_el asiento_disp%ROWTYPE;
+BEGIN
+    OPEN asiento_disp;
+    FETCH asiento_disp INTO asiento_el;
+    IF asiento_disp%FOUND THEN
+        INSERT INTO vuelo_plan (vp_tipo,
+        vp_modo,
+        vp_status,
+        vp_pv_id,
+        vp_asi_id,
+        vp_vu_id) VALUES (tipo, modo, reg_sta(status), id_plan_viaje, asiento_el.asi_id, id_vuelo);
+    END IF;
+END;
+/
+CREATE OR REPLACE PROCEDURE generar_reserva_Hotel
+(
+    fecha_in DATE,
+    fecha_out DATE,
+    id_plan_viaje number,
+    id_tipo_habitacion number
+)
+IS
+CURSOR habitacion_disp IS SELECT * FROM habitacion WHERE ha_th_id = id_tipo_habitacion;
+habitacion_el habitacion_disp%ROWTYPE;
+BEGIN
+    OPEN habitacion_disp;
+    FETCH habitacion_disp INTO habitacion_el;
+    IF habitacion_disp%FOUND THEN
+        INSERT INTO (rh_fecha,
+            rh_precio_total,
+            rh_status,
+            rh_pv_id,
+            rh_ha_id,
+            rh_puntuacion) VALUES (reg_ope(fecha_in, fecha_out), 0, 'ACT', id_plan_viaje, habitacion_el.ha_id, 0);
+    END IF;
+END;
+/
+CREATE OR REPLACE PROCEDURE generar_reserva_auto
+(
+    fecha_in DATE,
+    fecha_out DATE,
+    id_plan_viaje number,
+    id_modelo number,
+    id_isp number,
+    recogida reg_loc,
+    entrega reg_loc
+)
+IS
+CURSOR auto_disp IS SELECT * FROM automovil WHERE au_mau_id = id_modelo AND au_asp_id = id_isp;
+auto_el auto_disp%ROWTYPE;
+BEGIN
+    OPEN auto_disp;
+    FETCH auto_disp INTO auto_el;
+    IF auto_disp%FOUND THEN
+        INSERT INTO alquiler_auto (aa_dir_recogida, aa_dir_devolucion, aa_fecha, aa_precio_total, aa_status, aa_pv_id, aa_au_id) 
+        VALUES (recogida, entrega, reg_ope(fecha_in, fecha_out), 0, reg_sta('ACT'), id_plan_viaje, auto_el.au_id);
+    END IF;
+END;
+/
+CREATE OR REPLACE PROCEDURE generar_contrato
+(
+    cantidad number,
+    id_plan_viaje number,
+    id_seguro number
+)
+IS
+BEGIN
+    INSERT INTO contrato (co_cantidad, co_pv_id, co_se_id) VALUES (cantidad, id_plan_viaje, id_seguro);
+END;
+
+
+
+
--- SRC/triggers.sql
+++ SRC/triggers.sql
@@ -1,50 +1,117 @@
-create or replace trigger tri_vi_asp
-instead of insert on vi_asp
-for each row
-begin
-    DECLARE 
-    V_blob BLOB;
-    V_bfile BFILE;
-    BEGIN 
-        INSERT INTO alquiler_sp(asp_id, asp_nombre, asp_logo) VALUES (:new.asp_id, :new.asp_nombre, empty_blob()) RETURNING asp_logo INTO V_blob;
-        V_bfile := BFILENAME('IMGS_ASP', :new.asp_nombre||'.jpg');
-        DBMS_LOB.OPEN(V_bfile, DBMS_LOB.LOB_READONLY);
-        DBMS_LOB.LOADFROMFILE(V_blob, V_bfile, SYS.DBMS_LOB.GETLENGTH(V_bfile));
-        DBMS_LOB.CLOSE(V_bfile);
-        --COMMIT;
-    END;
-end;
+CREATE OR REPLACE TRIGGER validacion_reserva_vuelo
+BEFORE INSERT ON Vuelo_Plan
+FOR EACH ROW
+DECLARE
+CURSOR reg_vuelo IS SELECT * FROM vuelo WHERE vu_id = :new.vp_vu_id;
+CURSOR comp_reservas_ant IS SELECT * FROM vuelo JOIN vuelo_plan ON vuelo.vu_id = vuelo_plan.vp_vu_id WHERE :new.vp_pv_id = vuelo_plan.vp_pv_id;
+CURSOR asiento_ocupado IS SELECT count(*) as contador FROM Vuelo_Plan WHERE :new.vp_asi_id = vuelo_plan.vp_asi_id AND vuelo_plan.vp_vu_id = :new.vp_vu_id;
+reg_vue_el reg_vuelo%ROWTYPE;
+BEGIN
+    OPEN reg_vuelo;
+    FETCH reg_vuelo INTO reg_vue_el;
+    IF (reg_vue_el.vu_status.status <> 'ACT') THEN
+            raise_application_error(-1020, 'No se puede realizar la reservacion a un vuelo inactivo/invalido');
+    END IF;
+    OPEN comp_reservas_ant;
+    FOR reg_vue IN comp_reservas_ant LOOP
+        IF (reg_vue.vu_fecha.validar_solapamiento(reg_vue.vu_fecha.fecha_in, reg_vue.vu_fecha.fecha_out, reg_vue_el.vu_fecha.fecha_in, reg_vue_el.vu_fecha.fecha_out) = 1) THEN
+            raise_application_error(-1020, 'No se puede realizar la reservacion de un vuelo, cuando se tiene una reserva realizada en el mismo lapso de tiempo');
+        END IF;
+    END LOOP;
+    OPEN asiento_ocupado;
+    FOR asiento_ocu in asiento_ocupado LOOP
+        IF (asiento_ocu.contador > 1) THEN
+            raise_application_error(-1020, 'Asiento ocupado');
+        END IF;
+    END LOOP;
+END;
 /
-create or replace trigger tri_vi_mau
-instead of insert on vi_mau
-for each row
-begin
-    DECLARE 
-    V_blob BLOB;
-    V_bfile BFILE;
-    BEGIN 
-        INSERT INTO modelo_auto(mau_id, mau_nombre, mau_pasajeros, mau_m_id, mau_foto) VALUES (:new.mau_id, :new.mau_nombre, :new.mau_pasajeros, :new.mau_m_id, empty_blob()) RETURNING mau_foto INTO V_blob;
-        V_bfile := BFILENAME('IMGS_MAU', :new.mau_nombre||'.jpg');
-        DBMS_LOB.OPEN(V_bfile, DBMS_LOB.LOB_READONLY);
-        DBMS_LOB.LOADFROMFILE(V_blob, V_bfile, SYS.DBMS_LOB.GETLENGTH(V_bfile));
-        DBMS_LOB.CLOSE(V_bfile);
-        --COMMIT;
-    END;
-end;
+CREATE OR REPLACE TRIGGER validacion_reserva_vuelo_stats
+BEFORE UPDATE ON Vuelo_Plan
+FOR EACH ROW
+DECLARE
+Cursor vuelo_reg IS SELECT * FROM Vuelo WHERE Vuelo.vu_id = :new.vp_vu_id;
+vuelo_el vuelo_reg%ROWTYPE;
+BEGIN
+    OPEN vuelo_reg;
+    FETCH vuelo_reg INTO vuelo_el;
+    IF (:new.vp_status.status <> :old.vp_status.status) THEN
+        :new.vp_status.validar_cambio_status(0, :new.vp_pv_id, :new.vp_status.status);
+    END IF;
+END;
 /
-create or replace trigger tri_vi_ho
-instead of insert on vi_ho
-for each row
-begin
-    DECLARE 
-    V_blob BLOB;
-    V_bfile BFILE;
-    BEGIN 
-        INSERT INTO hotel(ho_id, ho_nombre, ho_puntuacion, ho_locacion, ho_foto) VALUES (:new.ho_id, :new.ho_nombre, :new.ho_puntuacion, :new.ho_locacion, empty_blob()) RETURNING ho_foto INTO V_blob;
-        V_bfile := BFILENAME('IMGS_HO', :new.ho_nombre||'.jpg');
-        DBMS_LOB.OPEN(V_bfile, DBMS_LOB.LOB_READONLY);
-        DBMS_LOB.LOADFROMFILE(V_blob, V_bfile, SYS.DBMS_LOB.GETLENGTH(V_bfile));
-        DBMS_LOB.CLOSE(V_bfile);
-        --COMMIT;
-    END;
-end;
+CREATE OR REPLACE TRIGGER validacion_reserva_habitacion
+BEFORE INSERT ON Reserva_hotel
+FOR EACH ROW
+DECLARE
+CURSOR reg_reserva_habitacion IS SELECT * FROM Reserva_Hotel WHERE:new.rh_ha_id = reserva_hotel.rh_ha_id;
+BEGIN
+    OPEN reg_reserva_habitacion;
+    IF (:new.rh_status.status <> 'ACT') THEN
+        raise_application_error(-1020, 'No se puede realizar la reservacion a una habitacion inactivo/invalido');
+    END IF;
+    FOR reserva IN reg_reserva_habitacion LOOP
+        IF (reserva.rh_fecha.validar_solapamiento(reserva.rh_fecha.fecha_in, reserva.rh_fecha.fecha_out, :new.rh_fecha.fecha_in, :new.rh_fecha.fecha_out) = 1) THEN
+            raise_application_error(-1020, 'No se puede realizar la reservacion a una habitacion cuando esta esta reservada');
+        END IF;
+    END LOOP;
+END;
+/
+CREATE OR REPLACE TRIGGER validacion_reserva_habitacion_stats
+BEFORE UPDATE ON Reserva_hotel
+FOR EACH ROW
+DECLARE
+BEGIN
+    IF (:new.rh_status.status <> :old.rh_status.status) THEN
+        :new.rh_status.validar_cambio_status(1, :new.rh_pv_id, :new.rh_status.status);
+    END IF;
+END;
+/
+CREATE OR REPLACE TRIGGER validacion_alquiler_auto
+BEFORE INSERT ON Alquiler_Auto
+FOR EACH ROW
+DECLARE
+CURSOR reg_alquiler_auto IS SELECT * FROM Alquiler_auto WHERE:new.aa_au_id = alquiler_auto.aa_au_id;
+BEGIN
+    OPEN reg_alquiler_auto;
+    IF (:new.aa_status.status <> 'ACT') THEN
+        raise_application_error(-1020, 'No se puede realizar la reservacion a una habitacion inactivo/invalido');
+    END IF;
+    FOR alquiler IN reg_alquiler_auto LOOP
+        IF (alquiler.aa_fecha.validar_solapamiento(alquiler.aa_fecha.fecha_in, alquiler.aa_fecha.fecha_out, :new.aa_fecha.fecha_in, :new.aa_fecha.fecha_out) = 1) THEN
+            raise_application_error(-1020, 'No se puede realizar la reservacion a una habitacion cuando esta esta reservada');
+        END IF;
+    END LOOP;
+END;
+/
+CREATE OR REPLACE TRIGGER validacion_alquiler_auto_stats
+BEFORE UPDATE ON Alquiler_Auto
+FOR EACH ROW
+DECLARE
+BEGIN
+    IF (:new.aa_status.status <> :old.aa_status.status) THEN
+        :new.aa_status.validar_cambio_status(2, :new.aa_pv_id, :new.aa_status.status);
+    END IF;
+END;
+/
+CREATE OR REPLACE TRIGGER validacion_nodos
+BEFORE INSERT ON nodo
+FOR EACH ROW
+DECLARE
+ Cursor nodo_registrado IS SELECT * FROM nodo where :new.no_vu_id = nodo.no_vu_id;
+ flag_or number:=1;
+ flag_des number:=1;
+BEGIN
+    OPEN nodo_Registrado;
+    FOR nodo_reg in nodo_registrado LOOP
+        if(nodo_reg.no_modo ='ORI') then
+            flag_or := flag_or -1;
+        end if;
+        if(nodo_reg.no_modo ='DES') then
+            flag_des := flag_des -1;
+        end if;
+        if (flag_or < 0 or flag_des < 0) then
+            raise_application_error(-1020, 'Mas de un nodo en una ruta no permitido');
+        end if;
+    END LOOP;
+END;
