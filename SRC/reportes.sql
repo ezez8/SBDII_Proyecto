14ccd7629a1e808220269e36264ebf2637452aaa
SatoshiKiddo <elesfuerzodeminami21@hotmail.com>
2020-01-06 11:44:49 -0400

    Arreglo reporte 11

--- SRC/reportes.sql
+++ SRC/reportes.sql
@@ -205,9 +205,9 @@ create or replace procedure repo11(cur out sys_refcursor, p_fecha_s varchar, p_f
 is 
 begin
     open cur for
-    select mau.mau_foto "Foto del automovil", BASE.* from
+    select mau.mau_foto "Foto del automovil", asp.asp_logo "Proveedor de servicio", BASE.* from
     (
-        select distinct A.mau_id "id modelo", A.mau_nombre "Marca-Modelo Automovil", C.asp_logo "Proveedor de servicio",
+        select distinct A.mau_id "id modelo", A.mau_nombre "Marca-Modelo Automovil",
         G.u_correo "Correo de usuario", D.aa_dir_recogida "Direccion de recogida",
         D.aa_dir_devolucion "Direccion de devolucion",
         D.aa_fecha.fecha_in "Fecha y hora de inicio",
@@ -223,7 +223,9 @@ begin
         join Usuario G on G.u_id = F.pu_u_id and G.u_correo = correo
         where D.aa_fecha.fecha_in >= to_date(p_fecha_s,'dd-mm-yyyy') and D.aa_fecha.fecha_in <= to_date(p_fecha_r,'dd-mm-yyyy')
     ) BASE
-    join modelo_auto mau on mau.mau_id = "id modelo";
+    join modelo_auto mau on mau.mau_id = "id modelo"
+    join automovil au on au.au_mau_id = mau.mau_id
+    join alquiler_sp asp on asp.asp_id = au.au_asp_id;
 end;
 /
 create or replace procedure repo12(cur out sys_refcursor, p_fecha_s varchar)
